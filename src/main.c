cca3b94ec97b1a438e7f1362cfbc3c3611e4d358
Alexander Mann <amann@c1r4p7.hive.fi>
2022-05-27 15:47:11 +0300

    instruments done

--- src/main.c
+++ src/main.c
@@ -25,11 +25,10 @@ int main(int argc, char **argv)
     audio_spec.channels = 1;
     audio_spec.samples = 1024;
     audio_spec.callback = NULL;
-    audio_device = SDL_OpenAudioDevice(
-        NULL, 0, &audio_spec, NULL, 0);
+    audio_device = SDL_OpenAudioDevice(NULL, 0, &audio_spec, NULL, 0);
     // pushing 3 seconds of samples to the audio buffer:
-    note.duration = 0.5;
-    note.frequency = 440.0;
+    note.duration = 3;
+    note.frequency = 220.0;
 	float tc;
     for (int i = 0; i < audio_spec.freq * note.duration; i++) {
 
@@ -37,13 +36,13 @@ int main(int argc, char **argv)
         // note: "5000" here is just gain so that we will hear something
        //int16_t sample = sin(i * 0.04) * 5000;
        tc = (float)i / (float)audio_spec.freq;
-       int16_t sample = sine_instrument(note.frequency, tc) * 5000;
-       //printf("%d %d %f %hd\n",i, audio_spec.freq, tc, sample);
-        //SDL_QueueAudio(audio_device, &sample, sizeof(int16_t));
+       int16_t sample = saw_instrument(note.frequency, tc) * 5000;
+	   printf("%d %d %f %hd\n",i, audio_spec.freq, tc, sample);
+       SDL_QueueAudio(audio_device, &sample, sizeof(int16_t));
     }
     // unpausing the audio device (starts playing):
     SDL_PauseAudioDevice(audio_device, 0);
-    //SDL_Delay(3000);
+    SDL_Delay(3000);
     SDL_CloseAudioDevice(audio_device);
     SDL_Quit();
 
--- src/notes.c
+++ src/notes.c
@@ -7,5 +7,47 @@ double sine_instrument(double frequency, float tc)
 
 double square_instrument(double frequency, float tc)
 {
-	return (sin(tc * frequency * 2.0 * M_PI));
+	if (sin(tc * frequency * 2.0 * M_PI) > 0)
+		return (1);
+	else
+		return (-1);
 }
+
+double triangle_instrument(double frequency, float tc)
+{
+	double res, x;
+	int	factor;
+
+	x = tc * frequency * 2.0 * M_PI;
+	res = sin(x);
+	res += ((pow(3, -2)) * -(sin(x * 3)));
+	factor = 5;
+	while (factor < 12)
+	{
+		res += ((pow(factor, -2)) * (sin(x * factor)));
+		factor += 2;
+	}
+	return res;
+}
+
+double saw_instrument(double frequency, float tc)
+{
+	double res, x;
+	int factor;
+
+	x = tc * frequency * 2.0 * M_PI;
+	res = sin(x);
+	factor = 2;
+	while (factor < 26)
+	{
+		if (factor % 2 == 0)
+			res -= ((1.0 / factor) * sin(x * factor));
+		else	
+			res += ((1.0 / factor) * sin(x * factor));
+		factor++;
+	}
+	return (res);
+}
+
+
+//sin_x + ((pow(3, -2)) * (0 - (sin_x * 3))) + ((pow(5, -2)) * (sin_x * 5)) + ((pow(7, -2)) * (sin_x * 7)) + ((pow(9, -2)) * (sin_x * 9)) + ((pow(11, -2)) * (sin_x * 11));
